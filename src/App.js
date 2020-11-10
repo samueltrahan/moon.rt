99aca3fadfaea27855e075befbc368fb31b8315e
Samuel Trahan <samtrahan91@gmail.com>
2020-11-09 19:09:43 -0600

    background and header set up

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './App.css';
+import Moon from './Assets/moon.png'
 
 export default function App() {
   return (
     <div>
-      Hello World
+      <div className="header">
+        <img className="moon" alt="" src={Moon}></img>
+        <h1 className="header-title">Moon</h1>
+        <p className="header-content">A free, fully responsive React site template</p>
+      </div>
     </div>
   )
 }
