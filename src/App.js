e2e69715a35fef8eda09d9ab0f76696b73edb164
Samuel Trahan <samtrahan91@gmail.com>
2020-11-10 08:29:13 -0600

    section header styling and trying to get active to work

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {BrowserRouter as Router} from 'react-router-dom';
 import './App.css';
-import Moon from './Assets/moon.png'
+import Moon from './Assets/moon-logo.png'
 import Section from './components/Section/Section';  
 
 export default function App() {
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,10 +3,24 @@ import './Header.css';
 import {Link} from 'react-router-dom'
 
 export default function Header() {
+
+  let linkContainer = document.getElementsById('section-header');
+
+  let btns = linkContainer.getElementsByClassName('section-links');
+
+  for(let i = 0; i < btns.length; i++) {
+    btns[i].addEventListener('click', function() {
+      let current = document.getElementsByClassName("active");
+      current[0].className = current[0].className.replace(" active", "");
+      this.className += " active";
+    })
+  }
+
+
   return (
-    <nav className="section-header">
+    <nav id="section-header">
       <Link role="button" className="section-links" to="/intro">Introduction</Link>
-      <Link role="button" className="section-links" to="/first">First Section</Link>
+      <Link role="button" className="section-links active" to="/first">First Section</Link>
       <Link role="button" className="section-links" to="/second">Second Section</Link>
       <Link role="button" className="section-links" to="/getstarted">Get Started</Link>
     </nav>
