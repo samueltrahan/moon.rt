f860d462823c1e93cdb1299b340887a52b1db414
Samuel Trahan <samtrahan91@gmail.com>
2020-11-10 19:23:52 -0600

    content section styling

--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 import './Content.css';
+import Pic02 from '../../Assets/circle.png';
 
 export default function Content() {
   return (
   <div className="content-section">
     <div className="explore">
-      <h1>Explore our world</h1>
+      <h1 className="explore-title">Explore our world</h1>
+      <p>Welcome to Moon, come explore our world of development. This template will take your business or portfolio to the next level!</p>
+      <Link role="button" to="/explore" className="learn-more-btn">Learn More</Link>
+    </div>
+    <div className="content-picture">
+    <img alt="" className="picture" src={Pic02}/>
     </div>
   </div>
 
