1f93b468e48bee3e404be23b1338abce074514a6
Samuel Trahan <samtrahan91@gmail.com>
2020-11-11 10:32:44 -0600

    section one styling complete

--- src/components/ContentOne/ContentOne.js
+++ src/components/ContentOne/ContentOne.js
@@ -1,8 +1,9 @@
-import React from 'react';
-import './ContentOne.css';
-import Pic03 from '../../Assets/first-section.png'
-import Pic04 from '../../Assets/second-section.png'
-import Pic05 from '../../Assets/third-section.png'
+import React from "react";
+import {Link} from 'react-router-dom';
+import "./ContentOne.css";
+import Pic03 from "../../Assets/first-section.png";
+import Pic04 from "../../Assets/second-section.png";
+import Pic05 from "../../Assets/third-section.png";
 
 export default function ContentOne() {
   return (
@@ -11,14 +12,32 @@ export default function ContentOne() {
       <div className="first-section-card">
         <div className="first-card">
           <img className="circle-picture" alt="" src={Pic03}></img>
+          <p className="title">Build Your Website</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque hic
+            id eligendi optio officia aspernatur deserunt.{" "}
+          </p>
         </div>
         <div className="second-card">
-        <img className="circle-picture" alt="" src={Pic04}></img>
+          <img className="circle-picture" alt="" src={Pic04}></img>
+          <p className="title">Documents on Build</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque hic
+            id eligendi optio officia aspernatur deserunt.{" "}
+          </p>
         </div>
         <div className="third-card">
-        <img className="circle-picture" alt="" src={Pic05}></img>
+          <img className="circle-picture" alt="" src={Pic05}></img>
+          <p className="title">Diamond Build</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque hic
+            id eligendi optio officia aspernatur deserunt.{" "}
+          </p>
+        </div>
+        <div className="section-one-btn">
+        <Link role="button" to="/explore" id="section-one-btn" >Learn More</Link>
         </div>
       </div>
     </div>
-  )
+  );
 }
