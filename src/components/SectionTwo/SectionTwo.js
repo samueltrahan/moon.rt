22439ffe5915ad0be8c42c0b8a1342da4c558541
Samuel Trahan <samtrahan91@gmail.com>
2020-11-12 10:47:22 -0600

    some styling in section two

--- src/components/SectionTwo/SectionTwo.js
+++ src/components/SectionTwo/SectionTwo.js
@@ -3,8 +3,20 @@ import './SectionTwo.css';
 
 export default function SectionTwo() {
   return (
-    <div id="section-two">
-      Section Two
+    <div id="second-section">
+      <div className="header-two">
+      <h1>Ipsum consequat</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores facere dolorem consequuntur quas temporibus animi nostrum. Corrupti voluptates et iusto? 
+      </p>
+      </div>
+      <div className="board">
+        <div id="etiam">Etiam</div>
+        <div id="magna">Magna</div>
+        <div id="tempus">Tempus</div>
+        <div id="aliquam">Aliquam</div>
+        <div id="nullam">Nullam</div>
+      </div>
     </div>
   )
 }
