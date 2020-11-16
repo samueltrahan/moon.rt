a96ac7748eef53e4f4b54e3d4c5c73de2976a97b
Samuel Trahan <samtrahan91@gmail.com>
2020-11-16 10:37:51 -0600

    link fix

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,5 @@
 import React from "react";
 import "./Header.css";
-import { Link } from "react-router-dom";
 
 export default function Header() {
   let btns = document.getElementsByClassName("section-links");
